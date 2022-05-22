7a379196f819e42a6ad6eaec79844777876a5109
theBridgesDev <47019174+theBridgesDev@users.noreply.github.com>
2022-05-22 09:10:03 -0400

    Add files via upload
    Removed logging per transaction and made the output more user friendly.

--- GasFees/GasFees.js
+++ GasFees/GasFees.js
@@ -14,15 +14,23 @@ const main = async () => {
     let gasPriceHex = history[i].gasPrice._hex // Parse out just the gas fee in hex
     let gasLimitHex = history[i].gasLimit._hex // Parse out just the gas limit in hex
     let gasPriceBN = ethers.BigNumber.from(gasPriceHex) // Convert gas fee from hex to wei
-    let gasLimitBN = ethers.BigNumber.from(gasLimitHex) // Convert gas .limit from hex to wei
+    let gasLimitBN = ethers.BigNumber.from(gasLimitHex) // Convert gas limit from hex to wei
     let gasPriceWei = gasPriceBN.toNumber() // Convert gas price wei from string to number
     let gasLimitWei = gasLimitBN.toNumber() // Convert gas limit wei from string to number
-    gasFee = gasFee + gasPriceWei + gasLimitWei // Increment gas fee by price and limit
+    let gasPriceEth = ethers.utils.formatEther(gasPriceWei) // Convert gas price from wei to eth
+    gasFee = gasFee + gasPriceEth * gasLimitWei // Increment gas fee by price and limit
 
-    console.log(`Tx ${i+1} gas fee ${gasPriceWei + gasLimitWei}`)
+    // console.log(`Tx ${i+1} gas fee ${gasPriceEth * gasLimitWei}`)
   }
-
-  console.log(`Total gas fee: ${ethers.utils.formatEther(gasFee)}\n`)
+  console.log(`\nGas data for ${address}`)
+  console.log(`Number of transactions: ${history.length}`)
+  console.log(`Average gas fee: ${gasFee / history.length}\n`)
+  console.log(`Total gas fee: ${gasFee}`)
+  console.log(`USD Value (@ $2000/ETH): ${gasFee * 2000}`)
+  console.log(`USD Value (@ $4000/ETH): ${gasFee * 4000}`)
+  console.log(`USD Value (@ $6000/ETH): ${gasFee * 6000}`)
+  console.log(`USD Value (@ $8000/ETH): ${gasFee * 8000}`)
+  console.log(`USD Value (@ $10000/ETH): ${gasFee * 10000}\n`)
 }
 
 main()
